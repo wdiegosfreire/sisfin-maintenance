30838a6ff46ca075ec13c84f32a1b59d629b1fb8
Walter Diego de Souza Freire <wdiegosfreire@yahoo.com.br>
2021-11-02 11:50:51 -0300

    Project sisfin-maintenance initial commit.

---
+++ src/main/java/br/com/dfdevforge/sisfinmaintenance/SisfinMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package br.com.dfdevforge.sisfinmaintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisfinMaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisfinMaintenanceApplication.class, args);
+	}
+
+}
