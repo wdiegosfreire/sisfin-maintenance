a5d35a1cb9e6a3a445f9b292808e2650f4c49500
Walter Diego de Souza Freire <wdiegosfreire@yahoo.com.br>
2022-02-23 18:27:13 -0300

    A lot of changes...

--- src/main/java/br/com/dfdevforge/sisfinmaintenance/resources/UserResource.java
+++ src/main/java/br/com/dfdevforge/sisfinmaintenance/resources/UserResource.java
@@ -4,8 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,14 +14,12 @@ import br.com.dfdevforge.common.exceptions.BaseException;
 import br.com.dfdevforge.sisfinmaintenance.entities.UserEntity;
 import br.com.dfdevforge.sisfinmaintenance.services.user.UserExecuteAuthenticationService;
 import br.com.dfdevforge.sisfinmaintenance.services.user.UserExecuteRegistrationService;
-import br.com.dfdevforge.sisfinmaintenance.services.user.UserExecuteSearchService;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
 	private ResourceDataEntity resourceData = new ResourceDataEntity();
 
-	@Autowired private UserExecuteSearchService userExecuteSearchService;
 	@Autowired private UserExecuteRegistrationService userExecuteRegistrationService;
 	@Autowired private UserExecuteAuthenticationService userExecuteAuthenticationService;
 
@@ -47,17 +43,6 @@ public class UserResource {
 		this.userExecuteRegistrationService.setEntity(user);
 		this.resourceData.setMap(this.userExecuteRegistrationService.execute());
 
-		return ResponseEntity.ok(this.resourceData);
-	}
-
-	@GetMapping(value = "/{identity}")
-	public ResponseEntity<ResourceDataEntity> findByIdentity(@PathVariable long identity) throws BaseException {
-		UserEntity user = new UserEntity();
-		user.setIdentity(identity);
-
-		this.userExecuteSearchService.setEntity(user);
-		this.resourceData.setMap(this.userExecuteSearchService.execute());
-		
 		return ResponseEntity.ok(this.resourceData);
 	}
 }
