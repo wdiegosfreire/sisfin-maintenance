60e88bda011562b71eb8020b018a89e205c813c3
Walter Diego de Souza Freire <wdiegosfreire@yahoo.com.br>
2022-03-22 10:43:30 -0300

    Created classes "TestConfig.java" and "UserExecuteAuthenticationServiceTest.java".

---
+++ src/test/java/br/com/dfdevforge/sisfinmaintenance/TestConfig.java
@@ -0,0 +1,8 @@
+package br.com.dfdevforge.sisfinmaintenance;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TestConfig {}
