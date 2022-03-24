a72f0604f673a645f033730686bbaa1fe48c59c0
Walter Diego de Souza Freire <wdiegosfreire@yahoo.com.br>
2022-03-24 14:54:59 -0300

    Added information in file "ImRunning.java"; created files "DateUtils.java" and "DatePatternEnum.java".

--- src/main/java/br/com/dfdevforge/sisfinmaintenance/ImRunning.java
+++ src/main/java/br/com/dfdevforge/sisfinmaintenance/ImRunning.java
@@ -3,10 +3,14 @@ package br.com.dfdevforge.sisfinmaintenance;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.dfdevforge.common.enums.DatePatternEnum;
+import br.com.dfdevforge.common.utils.Utils;
 
 @RestController
 @RequestMapping(value = "/imrunning")
@@ -14,15 +18,25 @@ public class ImRunning {
 	private static final String BREAK = "<br>";
 
 	@Autowired
-	BuildProperties buildProperties;
+	private BuildProperties buildProperties;
+
+	@Value("${server.port}")
+	private String serverPort;
+
+	@Value("${spring.profiles.active}")
+	private String springProfilesActive;
+
+	@Value("${spring.application.name}")
+	private String springApplicationName;
 
 	@GetMapping
 	public String imRunning() {
 		StringBuilder statusMessage = new StringBuilder();
 
-		statusMessage.append("Project: Sisfin Maintenance" + BREAK);
-		statusMessage.append("Version: " + String.format("v%s", this.buildProperties.getVersion()) + BREAK);
-		statusMessage.append("Check Date: " + new Date());
+		statusMessage.append("application.name: " + springApplicationName + BREAK);
+		statusMessage.append("application.profile: " + springProfilesActive + BREAK);
+		statusMessage.append("application.version: " + String.format("%s", this.buildProperties.getVersion()) + BREAK);
+		statusMessage.append("application.now: " + Utils.date.format(new Date(), DatePatternEnum.EN_US_DASH_DIA_MES_ANO_HOR_MIN_SEG));
 
 		return statusMessage.toString();
 	}
