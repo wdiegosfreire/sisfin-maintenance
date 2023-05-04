abb32adeb49f2857574a65d2ea395cd9fc71b5ad
Walter Diego de Souza Freire <wdiegosfreire@yahoo.com.br>
2023-05-04 11:28:42 -0300

    Project dockerization in progress.

--- src/main/java/br/com/dfdevforge/common/utils/Utils.java
+++ src/main/java/br/com/dfdevforge/common/utils/Utils.java
@@ -6,4 +6,6 @@ public class Utils {
 	public static final LogUtils log = new LogUtils();
 	public static final DateUtils date = new DateUtils();
 	public static final ValueUtils value = new ValueUtils();
+	public static final DecryptUtils decrypt = new DecryptUtils();
+	public static final EncryptUtils encrypt = new EncryptUtils();
 }
