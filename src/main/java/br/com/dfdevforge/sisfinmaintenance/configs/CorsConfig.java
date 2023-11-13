46eb09481e71244e01eb27d137fb5da38233d7a1
Walter Diego de Souza Freire <wdiegosfreire@yahoo.com.br>
2023-11-13 19:38:34 -0300

    IP privado do servidor de font adicionado à configuração de cors.

--- src/main/java/br/com/dfdevforge/sisfinmaintenance/configs/CorsConfig.java
+++ src/main/java/br/com/dfdevforge/sisfinmaintenance/configs/CorsConfig.java
@@ -21,7 +21,7 @@ public class CorsConfig extends WebSecurityConfigurerAdapter {
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:8180", "https://sisfin-frontend.herokuapp.com", "https://sisfin-transaction.herokuapp.com"));
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:8180", "http://172.31.43.236"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
 		configuration.setAllowedHeaders(Arrays.asList("*"));
 		configuration.setAllowCredentials(false);
